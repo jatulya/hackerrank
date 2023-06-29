c2d29a15e0318dc1d0322582874a70eff03b8f6e
J-Atulya <atulyajayakrishnan@gmail.com>
2023-06-29 23:20:20 +0530

    Changes I had forgotten were done

---
+++ java/currency.java
@@ -0,0 +1,20 @@
+//getcurrencyinstance() method is static -> no objects needed to call the method. just the class name
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Locale.*;
+import java.text.*;
+public class currency {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+        String in = NumberFormat.getCurrencyInstance(new Locale("en", "in")).format(payment);
+        System.out.println("US: " + us);
+        System.out.println("India: " + in);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);        
+    }   
+}
--- python/itemprice.py
+++ python/itemprice.py
@@ -19,6 +19,19 @@ for k, val in od.items():
     print(k, val)
         
     
-        
+d2 = list(map(int, input().split()))
+d1 = list(map(int, input().split()))
+
+if d2[2]-d1[2] > 0:
+    fine = 10000
+elif d2[1]-d1[1] > 0:
+    fine = 500 * (d2[1]-d1[1])
+elif d2[0]-d1[0]:
+    fine = 15 * (d2[0]-d1[0])
+else:
+    fine = 0
+    
+print(fine)
+
     
     
