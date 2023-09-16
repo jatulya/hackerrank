582fb2981d369dbf2eac44a9b86124d42317a890
J-Atulya <atulyajayakrishnan@gmail.com>
2023-09-16 23:56:25 +0530

    .

---
+++ DS-c/subset.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int divisible(int a[], int n, int k)
+{
+    int l=0, i;
+    return l;
+}
+
+int main() 
+{
+    int ele,i,n, k;
+    cin>>n>>k; 
+    int b[4]; 
+    for (i=0; i<n; i++)
+        b[i] = 0;  
+    for (i=0; i<n; i++)
+    {
+        cin>>ele;
+        b[ele%k]++;
+    }
+    
+        
+
+    
+
+
+}
